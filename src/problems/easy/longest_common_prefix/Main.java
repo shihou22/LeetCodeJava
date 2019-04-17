fe56f766e2fb64597243d6dd027a4574d831f010
shihou22 <pc9801bx2@gmail.com>
2019-04-18 00:50:02 +0900

    add

--- src/problems/easy/longest_common_prefix/Main.java
+++ src/problems/easy/longest_common_prefix/Main.java
@@ -6,26 +6,43 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		String res = new Main().longestCommonPrefix(new String[] { "flower", "flow", "flight" });
+		String res = "";
+		res = new Main().longestCommonPrefix(new String[] { "flower", "flow", "flight" });
+		System.out.println(res);
+		res = new Main().longestCommonPrefix(new String[] {});
+		System.out.println(res);
+		res = new Main().longestCommonPrefix(new String[] { "aca", "cba" });
 		System.out.println(res);
 	}
 
 	public String longestCommonPrefix(String[] strs) {
 
 		Arrays.sort(strs, (x, y) -> {
-
+			if (x.length() < y.length()) {
+				return -1;
+			} else if (x.length() > y.length()) {
+				return 1;
+			} else {
+				return x.compareTo(y);
+			}
 		});
 
+		if (strs.length == 0) {
+			return "";
+		}
+
 		StringBuilder builder = new StringBuilder();
+		int appended = 0;
 		for (int i = 0; i < strs[0].length(); i++) {
 			boolean canAppend = true;
-			for (int j = 1; j < strs.length; j++) {
+			for (int j = 1; j < strs.length && canAppend; j++) {
 				canAppend = strs[0].charAt(i) == strs[j].charAt(i);
-				if (!canAppend)
-					break;
 			}
-			if (canAppend) {
+			if (canAppend && appended == i) {
 				builder.append(strs[0].charAt(i));
+				appended++;
+			} else {
+				break;
 			}
 		}
 		return builder.toString();
