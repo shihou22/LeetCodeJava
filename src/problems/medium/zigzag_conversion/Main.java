5cec9ccc2d4a0eba8dbac09233dceb7b75f26411
shihou22 <pc9801bx2@gmail.com>
2019-04-18 15:46:42 +0900

    zig zag

--- src/problems/medium/zigzag_conversion/Main.java
+++ src/problems/medium/zigzag_conversion/Main.java
@@ -1,17 +1,53 @@
 package problems.medium.zigzag_conversion;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		boolean res = new Main().isValid("()");
+		String res = "";
+				 res= new Main().convert("PAYPALISHIRING", 3);
+				System.out.println(res);
+				res = new Main().convert("PAYPALISHIRING", 4);
+				System.out.println(res);
+		res = new Main().convert("AB", 1);
 		System.out.println(res);
 	}
 
-	public boolean isValid(String s) {
+	public String convert(String s, int numRows) {
 
-		return true;
+		if (numRows == 1) {
+			return s;
+		}
+
+		Map<Integer, StringBuilder> wk = new HashMap<Integer, StringBuilder>();
+		for (int i = 0; i < numRows; i++) {
+			wk.put(i, new StringBuilder());
+		}
+
+		boolean reverse = true;
+		int key = 0;
+		for (int i = 0; i < s.length(); i++) {
+			wk.get(key).append(s.charAt(i));
+			if (key == numRows - 1 || key == 0) {
+				reverse = !reverse;
+			}
+			if (reverse) {
+				key--;
+			} else {
+				key++;
+			}
+
+		}
+
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < numRows; i++) {
+			builder.append(wk.get(i));
+		}
+
+		return builder.toString();
 	}
 
 }
