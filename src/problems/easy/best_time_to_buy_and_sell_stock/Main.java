45cd3aa0873c00bf3960af184cfea575cb51cfd1
shihou22 <pc9801bx2@gmail.com>
2019-04-22 08:31:22 +0900

    best time to buy and sell stock ii

--- src/problems/easy/best_time_to_buy_and_sell_stock/Main.java
+++ src/problems/easy/best_time_to_buy_and_sell_stock/Main.java
@@ -7,41 +7,40 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int res = 0;
 		int[] wk;
+		//		wk = new int[] { 1, 2, 3, 4, 5 };
+		//		res = new Main().maxProfit(wk);//4
+		//		System.out.println(res);
+		//
+		//		wk = new int[] { 7, 6, 4, 3, 1 };
+		//		res = new Main().maxProfit(wk);//0
+		//		System.out.println(res);
+
 		wk = new int[] { 7, 1, 5, 3, 6, 4 };
-		res = new Main().maxProfit(wk);//5
+		res = new Main().maxProfit(wk);//7
 		System.out.println(res);
 
-		wk = new int[] { 7, 6, 4, 3, 1 };
-		res = new Main().maxProfit(wk);//0
-		System.out.println(res);
-
-		wk = new int[] { 2, 1, 4 };
-		res = new Main().maxProfit(wk);//2
-		System.out.println(res);
 	}
 
 	public int maxProfit(int[] prices) {
 
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
 		int res = 0;
+		int ret = 0;
 		int minPrice = Integer.MAX_VALUE - 1;
+		int prev = prices[0];
 		for (int i = 0; i < prices.length; i++) {
 			minPrice = Integer.min(minPrice, prices[i]);
 			res = Integer.max(res, prices[i] - minPrice);
-		}
-		return res;
-	}
-
-	public int maxProfitOrigin(int[] prices) {
-
-		int res = 0;
-		for (int i = 0; i < prices.length; i++) {
-			for (int j = i; j < prices.length; j++) {
-				if (prices[i] >= prices[j]) {
-					continue;
-				}
-				res = Integer.max(res, prices[j] - prices[i]);
+			if (prev > prices[i] || i == prices.length - 1) {
+				ret += res;
+				res = 0;
+				minPrice = prices[i];
 			}
+			prev = prices[i];
 		}
-		return res;
+		return ret;
 	}
+
 }
