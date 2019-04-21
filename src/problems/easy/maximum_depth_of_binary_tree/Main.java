653f5e2a6efe133ded1bc2b083b3e59032d2e735
shihou22 <pc9801bx2@gmail.com>
2019-04-21 21:44:03 +0900

    maximum depth

---
+++ src/problems/easy/maximum_depth_of_binary_tree/Main.java
@@ -0,0 +1,93 @@
+package problems.easy.maximum_depth_of_binary_tree;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		int res = 0;
+		TreeNode wk = null;
+
+		//		wk = new TreeNode(1);
+		//		wk.left = new TreeNode(2);
+		//		wk.right = new TreeNode(2);
+		//		wk.left.left = new TreeNode(3);
+		//		wk.right.left = new TreeNode(3);
+		//		res = new Main().maxDepth(wk);//false
+		//		System.out.println(res);
+		//
+		//		wk = new TreeNode(1);
+		//		wk.left = new TreeNode(2);
+		//		wk.right = new TreeNode(2);
+		//		wk.left.left = new TreeNode(3);
+		//		wk.right.right = new TreeNode(3);
+		//		res = new Main().maxDepth(wk);//true
+		//		System.out.println(res);
+		//
+		//		wk = new TreeNode(1);
+		//		wk.left = new TreeNode(2);
+		//		wk.right = new TreeNode(2);
+		//		wk.left.left = null;
+		//		wk.left.right = new TreeNode(3);
+		//		wk.right.left = new TreeNode(3);
+		//		res = new Main().maxDepth(wk);//true
+		//		System.out.println(res);
+		//
+		//		wk = new TreeNode(1);
+		//		wk.left = new TreeNode(2);
+		//		wk.right = new TreeNode(3);
+		//		res = new Main().maxDepth(wk);//false
+		//		System.out.println(res);
+
+		wk = new TreeNode(2);
+		wk.left = new TreeNode(3);
+		wk.right = new TreeNode(3);
+		wk.left.left = new TreeNode(4);
+		wk.left.right = new TreeNode(5);
+		wk.right.left = new TreeNode(4);
+		wk.right.right = new TreeNode(5);
+		res = new Main().maxDepth(wk);//false
+		System.out.println(res);
+	}
+
+	public int maxDepth(TreeNode root) {
+
+		if (root == null) {
+			return 0;
+		}
+		return 1 + Integer.max(maxDepth(root.left), maxDepth(root.right));
+	}
+
+	public int maxDepthOrigin(TreeNode root) {
+
+		if (root == null) {
+			return 0;
+		}
+		return chkDepthOrigin(root.left, root.right, 1);
+	}
+
+	private int chkDepthOrigin(TreeNode left, TreeNode right, int depth) {
+		if (left == null && right == null) {
+			return depth;
+		}
+		int res = depth;
+		if (left != null) {
+			res = Integer.max(chkDepthOrigin(left.left, left.right, depth + 1), res);
+		}
+		if (right != null) {
+			res = Integer.max(chkDepthOrigin(right.left, right.right, depth + 1), res);
+		}
+		return res;
+	}
+
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+}
